cbbd2959994860f19383aa799894f0575c856ea8
chencheng <sorrycc@gmail.com>
2014-06-23 14:57:05 +0800

    add glob support for pkg.output

--- test/spm.js
+++ test/spm.js
@@ -66,8 +66,8 @@ describe('Father.SpmPackage', function() {
 
   it('output', function() {
     var pkg = getPackage('output');
-    pkg.output.should.eql(['a.js', 'b.js']);
-    Object.keys(pkg.files).should.eql(['index.js','a.js', 'a1.js', 'b.js', 'b1.js']);
+    pkg.output.should.eql(['a.js', 'b.js', './glob/*.js']);
+    Object.keys(pkg.files).should.eql(['index.js','a.js', 'a1.js', 'b.js', 'b1.js', 'glob/c.js', 'c1.js']);
   });
 
   it('resolve deps', function() {
