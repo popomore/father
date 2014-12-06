e73300aed7f740c4ee65d016de79f67c977f1cd0
popomore <sakura9515@gmail.com>
2014-12-06 20:02:48 +0800

    feat(package)  api change
    - change: get -> getPackage
    - change: set -> setPackage
    - new: getFile
    - new: getFiles
    
    Closes #23

--- test/spm.test.js
+++ test/spm.test.js
@@ -19,43 +19,43 @@ describe('Father.SpmPackage', function() {
     pkg.version.should.eql('1.0.0');
     pkg.files['a.js'].dependencies.should.eql(['b', './b.js', 'd', './a.json', './a.handlebars']);
     pkg.files['b.js'].dependencies.should.eql(['b', 'c']);
-    pkgDeps['b'].should.eql(pkg.get('b@1.1.0'));
-    pkgDeps['c'].should.eql(pkg.get('c@1.1.1'));
-    pkgDeps['d'].should.eql(pkg.get('d@0.1.1'));
-    pkgDevDeps['e'].should.eql(pkg.get('e@1.1.0'));
+    pkgDeps['b'].should.eql(pkg.getPackage('b@1.1.0'));
+    pkgDeps['c'].should.eql(pkg.getPackage('c@1.1.1'));
+    pkgDeps['d'].should.eql(pkg.getPackage('d@0.1.1'));
+    pkgDevDeps['e'].should.eql(pkg.getPackage('e@1.1.0'));
 
-    var b = pkg.get('b@1.1.0');
+    var b = pkg.getPackage('b@1.1.0');
     b.fileCache.should.equal(pkg.fileCache);
     var bDeps = b.dependencies;
     b.main.should.eql('src/b.js');
     b.name.should.eql('b');
     b.version.should.eql('1.1.0');
     b.files['src/b.js'].dependencies.should.eql(['c', './b.tpl']);
-    bDeps['c'].should.eql(pkg.get('c@1.1.1'));
-    bDeps['d'].should.eql(pkg.get('d@0.1.0'));
+    bDeps['c'].should.eql(pkg.getPackage('c@1.1.1'));
+    bDeps['d'].should.eql(pkg.getPackage('d@0.1.0'));
 
-    var c = pkg.get('c@1.1.1');
+    var c = pkg.getPackage('c@1.1.1');
     c.fileCache.should.equal(pkg.fileCache);
     c.main.should.eql('index.js');
     c.name.should.eql('c');
     c.version.should.eql('1.1.1');
     c.files['index.js'].dependencies.should.eql(['d']);
 
-    var d1 = pkg.get('d@0.1.0');
+    var d1 = pkg.getPackage('d@0.1.0');
     d1.fileCache.should.equal(pkg.fileCache);
     d1.main.should.eql('index.js');
     d1.name.should.eql('d');
     d1.version.should.eql('0.1.0');
     d1.files['index.js'].dependencies.should.eql([]);
 
-    var d2 = pkg.get('d@0.1.1');
+    var d2 = pkg.getPackage('d@0.1.1');
     d2.fileCache.should.equal(pkg.fileCache);
     d2.main.should.eql('index.js');
     d2.name.should.eql('d');
     d2.version.should.eql('0.1.1');
     d2.files['index.js'].dependencies.should.eql([]);
 
-    var e = pkg.get('e@1.1.0');
+    var e = pkg.getPackage('e@1.1.0');
     e.fileCache.should.equal(pkg.fileCache);
     e.main.should.eql('src/e.js');
     e.name.should.eql('e');
@@ -86,7 +86,7 @@ describe('Father.SpmPackage', function() {
     var pkg = getPackage('other-entry', {entry: ['a.js']});
     Object.keys(pkg.files).should.eql(['index.js', 'a.js', 'b.js']);
 
-    var pkgB = pkg.get('b@1.0.0');
+    var pkgB = pkg.getPackage('b@1.0.0');
     Object.keys(pkgB.files).should.eql(['index.js', 'a.js']);
   });
 
@@ -94,7 +94,7 @@ describe('Father.SpmPackage', function() {
     var pkg = getPackage('other-entry', {entry: ['./glob/*.js']});
     Object.keys(pkg.files).should.eql(['index.js', 'glob/c.js', 'b.js']);
 
-    var pkgB = pkg.get('b@1.0.0');
+    var pkgB = pkg.getPackage('b@1.0.0');
     Object.keys(pkgB.files).should.eql(['index.js', 'a.js']);
   });
 
@@ -142,37 +142,37 @@ describe('Father.SpmPackage', function() {
     var pkg = getPackage('cascade');
     var pkgDeps = pkg.dependencies;
     Object.keys(pkgDeps).should.eql(['b']);
-    pkgDeps['b'].should.eql(pkg.get('b@1.1.0'));
+    pkgDeps['b'].should.eql(pkg.getPackage('b@1.1.0'));
     pkg.files['a.js'].dependencies.should.eql(['b']);
   });
 
-  it('get method', function() {
+  it('getPackage method', function() {
     var pkg = getPackage('normal');
-    var b = pkg.get('b@1.1.0');
+    var b = pkg.getPackage('b@1.1.0');
     b.main.should.eql('src/b.js');
     b.name.should.eql('b');
     b.version.should.eql('1.1.0');
     Object.keys(b.dependencies).should.eql(['d', 'c']);
     Object.keys(b.files).should.eql(['src/b.js', 'src/b.tpl']);
 
-    var self = pkg.get('a@1.0.0');
+    var self = pkg.getPackage('a@1.0.0');
     self.should.equal(pkg);
   });
 
-  xit('set method', function() {
+  xit('setPackage method', function() {
     var pkg = getPackage('normal');
-    pkg.set({id: 'b@1.1.0'});
-    pkg.get('b@1.1.0').should.eql({id: 'b@1.1.0'});
+    pkg.setPackage({id: 'b@1.1.0'});
+    pkg.getPackage('b@1.1.0').should.eql({id: 'b@1.1.0'});
 
-    pkg.set({id: 'b@1.1.0', name: 'b'});
-    pkg.get('b@1.1.0').should.eql({id: 'b@1.1.0'});
+    pkg.setPackage({id: 'b@1.1.0', name: 'b'});
+    pkg.getPackage('b@1.1.0').should.eql({id: 'b@1.1.0'});
   });
 
   it('getPackages method', function() {
     var pkg = getPackage('normal');
     var pkgs = pkg.getPackages();
 
-    var b = pkg.get('b@1.1.0');
+    var b = pkg.getPackage('b@1.1.0');
     pkgs['b@1.1.0'].should.equal(b);
 
     Object.keys(b.getPackages()).should.eql([
@@ -183,6 +183,36 @@ describe('Father.SpmPackage', function() {
       'e@1.1.0'
     ]);
   });
+
+  it('getFile method', function() {
+    var filepath = join(base, 'normal/sea-modules/d/0.1.0/index.js');
+    var pkg = getPackage('normal');
+    var file = pkg.getFile(filepath);
+    file.path.should.eql(filepath);
+    file.pkg.id.should.eql('d@0.1.0');
+    (pkg.getFile('notexists.js') === null).should.be.true;
+  });
+
+  it('getFiles method', function() {
+    var pkg = getPackage('normal');
+    var files = pkg.getFiles();
+    Object.keys(files).map(function(item) {
+      return item.replace(join(base, 'normal'), '');
+    }).should.eql([
+      '/sea-modules/d/0.1.0/index.js',
+      '/sea-modules/c/1.1.1/index.js',
+      '/sea-modules/b/1.1.0/src/b.js',
+      '/sea-modules/b/1.1.0/src/b.tpl',
+      '/sea-modules/d/0.1.1/index.js',
+      '/sea-modules/e/1.1.0/src/e.js',
+      '/a.js',
+      '/b.js',
+      '/a.json',
+      '/a.handlebars',
+      '/c.js'
+    ]);
+  });
+
 
   it('require other extension', function() {
     var pkg = getPackage('require-other-ext');
@@ -250,7 +280,7 @@ describe('Father.SpmPackage', function() {
       'no-exist': {name: 'a'}
     };
     should.exists(pkgB.dependencies['import-style']);
-    pkgB.dependencies['import-style'].should.equal(pkg.get('import-style@1.0.0'));
+    pkgB.dependencies['import-style'].should.equal(pkg.getPackage('import-style@1.0.0'));
 
     should.not.exists(pkgB.dependencies['no-exist']);
   });
@@ -279,13 +309,13 @@ describe('Father.SpmPackage', function() {
     deps['b/lib/b.js'].pkg.name.should.eql('b');
     deps['b/lib/b.js'].relative.should.eql('lib/b.js');
 
-    var pkgB = pkg.get('b@1.0.0');
+    var pkgB = pkg.getPackage('b@1.0.0');
     pkgB.files['lib/b.js'].dependencies.should.eql(['c/c.js']);
     should.exist(pkgB.files['a.js']);
     should.exist(pkgB.files['lib/b.js']);
     should.exist(pkgB.files['lib/index.js']);
 
-    var pkgC = pkg.get('c@1.0.0');
+    var pkgC = pkg.getPackage('c@1.0.0');
     should.exist(pkgC.files['c.js']);
   });
 
